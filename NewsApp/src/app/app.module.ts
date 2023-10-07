1213a0fa1e6c9aa8b3cf431bc5833c41028602f4
Rurangwa <chrisruu@yahoo.com>
2023-10-07 19:17:13 +0800

    update structure

--- NewsApp/src/app/app.module.ts
+++ NewsApp/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { AllNewsComponent } from './components/all-news/all-news.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllNewsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,HttpClientModule,FormsModule,
+    RouterModule.forRoot([{path:'all-news',component: AllNewsComponent},
+  ])
   ],
   providers: [],
   bootstrap: [AppComponent]
